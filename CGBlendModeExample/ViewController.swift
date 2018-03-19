b6b978851b7158813f09be8d2b1ae3f05ae327cd
René Kann <mail@renekann.de>
2018-03-19 16:33:07 +0100

    Initial Commit

---
+++ CGBlendModeExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  CGBlendModeExample
+//
+//  Created by René Kann on 19.03.18.
+//  Copyright © 2018 René Kann. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
